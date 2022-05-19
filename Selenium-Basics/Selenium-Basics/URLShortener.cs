96a4bf5fa0a031d8cd1f7a29ca1951b372750dd2
dimitrov93 <ts.dimitrov93@gmail.com>
2022-05-19 03:42:14 +0300

    More tests with selenium basics
    few more tests

--- Selenium-Basics/Selenium-Basics/URLShortener.cs
+++ Selenium-Basics/Selenium-Basics/URLShortener.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
 
 namespace Selenium_Basics
 {
@@ -11,15 +13,30 @@ namespace Selenium_Basics
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            if (!Debugger.IsAttached)
+            {
+                options.AddArgument("--headless");
+            }
+            
+            driver = new ChromeDriver(options);
         }
 
         [Test]
-        public void Test_Search_Wikipedia()
+        public void Test_Open_Page()
         {
             driver.Url = "https://shorturl.nakov.repl.co";
             var title = driver.Title;
             Assert.AreEqual(title, "URL Shortener");
+        }
+
+        [Test]
+        public void Test_Add_URL()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co";
+            driver.FindElement(By.CssSelector("body > header > a:nth-child(5)")).Click();
+            driver.FindElement(By.CssSelector("#url")).SendKeys("https://nakov.com");
+            driver.FindElement(By.CssSelector("body > main > form > table > tbody > tr:nth-child(3) > td > button")).Click();
         }
 
 
