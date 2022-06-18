81d292bd215ddf9d5641919be766678a2e528963
dimitrov93 <ts.dimitrov93@gmail.com>
2022-06-18 19:22:03 +0300

    Contact Book

---
+++ 10.Exam Prep4/ContactBook/ContactBook/UnitTest1.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+
+namespace ContactBook
+{
+    public class Tests
+    {
+        private AndroidDriver<AndroidElement> driver;
+        private AppiumOptions options;
+
+        private const string AppiumUrl = "http://127.0.0.1:4723/wd/hub";
+        private const string ContactBookUrl = "https://contactbook.nakov.repl.co/api";
+        private const string appLocation = @"C:\Users\Cvetomir\Desktop\apps\contactbook-androidclient.apk";
+
+        [SetUp]
+        public void OpenApp()
+        {
+            options = new AppiumOptions() { PlatformName = "Android" };
+            options.AddAdditionalCapability("app", appLocation);
+
+            driver = new AndroidDriver<AndroidElement>(new Uri(AppiumUrl), options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        public void ShutDownApp()
+        {
+            driver.CloseApp();
+            driver.Quit();
+        }
+
+        [Test]
+        public void Testing()
+        {
+            driver.FindElementById("contactbook.androidclient:id/editTextApiUrl").Clear();
+            driver.FindElementById("contactbook.androidclient:id/editTextApiUrl").SendKeys("https://contactbook.nakov.repl.co/api");
+            driver.FindElementById("contactbook.androidclient:id/buttonConnect").Click();
+            driver.FindElementById("contactbook.androidclient:id/editTextKeyword").SendKeys("Steve");
+            driver.FindElementById("contactbook.androidclient:id/buttonSearch").Click();
+
+            var firstName = driver.FindElementById("contactbook.androidclient:id/textViewFirstName").Text;
+            var lastName = driver.FindElementById("contactbook.androidclient:id/textViewLastName").Text;
+            var resultsText = driver.FindElementById("contactbook.androidclient:id/textViewSearchResult").Text;
+            Assert.That(firstName, Is.EqualTo("Steve"));
+            Assert.That(lastName, Is.EqualTo("Jobs"));
+            Assert.That(resultsText, Is.EqualTo("Contacts found: 1"));
+
+            driver.FindElementById("contactbook.androidclient:id/editTextKeyword").Clear();
+            driver.FindElementById("contactbook.androidclient:id/editTextKeyword").SendKeys("e");
+            driver.FindElementById("contactbook.androidclient:id/buttonSearch").Click();
+            var newResults = driver.FindElementById("contactbook.androidclient:id/textViewSearchResult").Text;
+
+            var allNames = driver.FindElementsById("contactbook.androidclient:id/textViewFirstName");
+            Console.WriteLine(allNames[0].Text);
+
+
+        }
+    }
+}
